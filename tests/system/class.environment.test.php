e92ba4fb920a3fa187b096d7a4805b27b4345c19
Heinz Wiesinger <heinz@m2mobi.com>
2012-11-18 22:13:21 +0100

    tests: Coding Style fixes.

--- tests/system/class.environment.test.php
+++ tests/system/class.environment.test.php
@@ -14,6 +14,7 @@
  */
 
 namespace Lunr;
+
 use PHPUnit_Framework_TestCase;
 use ReflectionClass;
 
@@ -42,7 +43,7 @@ class EnvironmentTest extends PHPUnit_Framework_TestCase
      */
     public function testL10nFiles()
     {
-        $file = dirname(__FILE__) . "/../statics/l10n/de_DE/LC_MESSAGES/Lunr.mo";
+        $file = dirname(__FILE__) . '/../statics/l10n/de_DE/LC_MESSAGES/Lunr.mo';
         $this->assertTrue(file_exists($file));
     }
 
