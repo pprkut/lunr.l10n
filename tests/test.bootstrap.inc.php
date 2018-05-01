b0261a212206dbfbba1ea8cd32dddc5ccf42d903
Heinz Wiesinger <pprkut@liwjatan.at>
2018-05-01 08:24:54 +0200

    General: Remove system folder from include path for tests
    Reviewed at https://reviews.lunr.nl/r/550/

--- tests/test.bootstrap.inc.php
+++ tests/test.bootstrap.inc.php
@@ -20,7 +20,6 @@ $base = __DIR__ . '/..';
 set_include_path(
     $base . '/src:' .
     $base . '/config:' .
-    $base . '/system:' .
     $base . '/tests:' .
     $base . '/tests/statics:' .
     get_include_path()
