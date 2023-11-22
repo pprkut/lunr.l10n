b526819f18b96fd80c5a34648f68605d3520cc06
Heinz Wiesinger <heinz.wiesinger@moveagency.com>
2023-11-22 16:51:44 +0100

    CI: Clean up include path construction for unit tests
    Reviewed at https://reviews.lunr.nl/r/1076/

--- tests/test.bootstrap.inc.php
+++ tests/test.bootstrap.inc.php
@@ -12,12 +12,14 @@
 
 $base = __DIR__ . '/..';
 
+// Define application config lookup path
+$paths = [
+    get_include_path(),
+    $base . '/src',
+];
+
 set_include_path(
-    $base . '/src:' .
-    $base . '/config:' .
-    $base . '/tests:' .
-    $base . '/tests/statics:' .
-    get_include_path()
+    implode(':', $paths)
 );
 
 if (file_exists($base . '/vendor/autoload.php') == TRUE)
